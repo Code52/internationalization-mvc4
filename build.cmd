f955076ee4e152020b88cd73cbe86f772e79f33c
Brendan Forster <me@brendanforster.com>
2012-02-26 23:47:49 +1100

    added Nuget Package Restore and readme file

--- build.cmd
+++ build.cmd
@@ -1,7 +1,14 @@
-
+@echo Off
 SETLOCAL
 SET VERSION=0.1
 SET NUGET=build\nuget.exe
+
+set config=%1
+if "%config%" == "" (
+   set config=Debug
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 mkdir artifacts
 
