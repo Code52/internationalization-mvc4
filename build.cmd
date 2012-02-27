a327785add6a21ea3552bcc4d7d7b3793ef588fe
David Christiansen <coding@davedoes.net>
2012-02-27 20:12:16 +0000

    Initial checkin / source layout

--- build.cmd
+++ build.cmd
@@ -9,11 +9,3 @@ if "%config%" == "" (
 )
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-
-mkdir artifacts
-
-FOR %%G IN (build\*.nuspec) DO (
-  %NUGET% pack %%G -Version %VERSION% -o artifacts
-)
-
-
---
+++ src/Code52.i18n.MVC4/NuSpec/tools/install.ps1
@@ -0,0 +1,5 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$path = [System.IO.Path]
+$readmefile = $path::Combine($path::GetDirectoryName($project.FileName), "App_Readme\Code52.i18n.MVC4.readme.txt")
+$DTE.ItemOperations.OpenFile($readmefile)
