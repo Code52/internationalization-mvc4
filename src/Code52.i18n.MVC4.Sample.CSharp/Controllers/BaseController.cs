4289a08068d9423b7755d6118f9c13e5ec94a0c7
Brendan Forster <me@brendanforster.com>
2012-02-28 22:36:42 +1100

    fixing a possible bug with MVC4 - ExecuteCore doesn't fire

--- src/Code52.i18n.MVC4.Sample.CSharp/Controllers/BaseController.cs
+++ src/Code52.i18n.MVC4.Sample.CSharp/Controllers/BaseController.cs
@@ -4,19 +4,22 @@ namespace Code52.i18n.MVCFour.Sample.CSharp.Controllers
     using System.Web;
     using System.Web.Mvc;
 
-    using Code52.i18n.MVCFour.Sample.CSharp.Code52Code;
+    using Code52Code;
 
     public abstract class BaseController : Controller {
-        protected override void ExecuteCore() {
+
+        protected override System.IAsyncResult BeginExecute(System.Web.Routing.RequestContext requestContext, System.AsyncCallback callback, object state)
+        {
             string cultureName = null;
-            HttpCookie cultureCookie = this.Request.Cookies["_culture"];
-            if (this.Request.UserLanguages != null)
-                cultureName = cultureCookie != null ? cultureCookie.Value : this.Request.UserLanguages[0];
-            cultureName = CultureHelper.GetImplementedCulture(cultureName); // This is safe
+            var request = requestContext.HttpContext.Request;
+            HttpCookie cultureCookie = request.Cookies["_culture"];
+            if (request.UserLanguages != null)
+                cultureName = cultureCookie != null ? cultureCookie.Value : request.UserLanguages[0];
+            cultureName = CultureHelper.GetImplementedCulture(cultureName);
             Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cultureName);
             Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
-            base.ExecuteCore();
-        }
 
+            return base.BeginExecute(requestContext, callback, state);
+        }
     }
 }
