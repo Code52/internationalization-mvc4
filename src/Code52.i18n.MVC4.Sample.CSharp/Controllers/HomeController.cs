0b6c5158d96018646b07a33d00707adf6dd16ccc
Brendan Forster <me@brendanforster.com>
2012-02-28 22:26:01 +1100

    added default message back

--- src/Code52.i18n.MVC4.Sample.CSharp/Controllers/HomeController.cs
+++ src/Code52.i18n.MVC4.Sample.CSharp/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
-﻿namespace Code52.i18n.MVCFour.Sample.CSharp.Controllers {
+﻿namespace Code52.i18n.MVCFour.Sample.CSharp.Controllers
+{
     using System.Web.Mvc;
 
-    public class HomeController : BaseController {
-        public ActionResult Index() {
-            this.ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            this.ViewBag.Message = "Modify this template to jump-start your ASP.NET application";
 
             return this.View();
         }
 
-        public ActionResult About() {
+        public ActionResult About()
+        {
             this.ViewBag.Message = "Your quintessential app description page.";
 
             return this.View();
         }
 
-        public ActionResult Contact() {
+        public ActionResult Contact()
+        {
             this.ViewBag.Message = "Your quintessential contact page.";
 
             return this.View();
