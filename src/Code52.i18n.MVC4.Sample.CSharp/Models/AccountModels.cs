17573f811b6e794d4b1825da51e7ecacdee56607
Brendan Forster <me@brendanforster.com>
2012-02-29 21:09:06 +1100

    using ModelMetadataProvider to inject resources into DataAnnotations

--- src/Code52.i18n.MVC4.Sample.CSharp/Models/AccountModels.cs
+++ src/Code52.i18n.MVC4.Sample.CSharp/Models/AccountModels.cs
@@ -1,58 +1,52 @@
-﻿namespace Code52.i18n.MVCFour.Sample.CSharp.Models {
+﻿namespace Code52.i18n.MVCFour.Sample.CSharp.Models
+{
     using System.ComponentModel.DataAnnotations;
     using System.Web.Mvc;
 
-    public class ChangePasswordModel {
+    public class ChangePasswordModel
+    {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Compare("NewPassword")]
         public string ConfirmPassword { get; set; }
     }
 
-    public class LoginModel {
+    public class LoginModel
+    {
         [Required]
-        [Display(Name = "User name")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
 
-    public class RegisterModel {
+    public class RegisterModel
+    {
         [Required]
-        [Display(Name = "User name")]
         public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.EmailAddress)]
-        [Display(Name = "Email address")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password")]
         public string ConfirmPassword { get; set; }
     }
 }
