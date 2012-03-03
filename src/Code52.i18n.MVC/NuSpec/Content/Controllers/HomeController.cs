ea2a07f1c6107fee5925c5204436456e4a793ef6
Brendan Forster <me@brendanforster.com>
2012-03-03 16:47:37 +1100

    started on package strucutre to test

---
+++ src/Code52.i18n.MVC/NuSpec/Content/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿namespace Code52.i18n.MVCFour.Sample.CSharp.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = Resources.Language.Index_Title;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = Resources.Language.About_Title;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = Resources.Language.Contact_Title;
+
+            return View();
+        }
+    }
+}
