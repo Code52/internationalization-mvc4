c5dc6e8a614ae30fc7c6664f24e605ec1ebfbff9
David Christiansen <coding@davedoes.net>
2012-03-03 14:43:22 +0000

    Creation of NuGet package structure - Started off from ShiftKey's work.

---
+++ src/Code52.i18n.MVC3.All/NuSpec/Content/App_Start/AppStart_Code52_i18n.cs.pp
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AppStart_Code52_i18n.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.AppStart_Code52_I18n), "Start")]
+namespace $rootnamespace$.App_Start {
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class AppStart_Code52_i18n {
+        public static void Start() {
+            RouteTable.Routes.MapRoute("Language", "i18n/Code52.i18n.language.js", new { controller = "Language", action = "Language" });
+        }
+    }
+}
