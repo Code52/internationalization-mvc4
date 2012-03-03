718d031d861059d54ddc0a410a665a5b63b3cc29
David Christiansen <coding@davedoes.net>
2012-03-03 19:07:50 +0000

    build parameters

--- build.cmd
+++ build.cmd
@@ -1,15 +1,19 @@
 @echo Off
 SETLOCAL
-SET VERSION=0.1.1
 SET NUGET=build\nuget.exe
 
 set config=%1
 if "%config%" == "" (
    set config=Debug
 )
+set version=%2
+if "%version%" == "" (
+   set version=0.1.1
+)
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
+rmdir artifacts /S /Q
 mkdir artifacts
 
 .\build\nuget.exe pack src\Code52.i18n\NuSpec\Code52.i18n.nuspec -Version %VERSION% -OutputDirectory artifacts
