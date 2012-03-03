b5e2771913b2fcc7a1fa5e7df495f426f4d9073a
Brendan Forster <me@brendanforster.com>
2012-03-03 22:17:25 +1100

    using a single web project

--- build.cmd
+++ build.cmd
@@ -13,9 +13,8 @@ if "%config%" == "" (
 mkdir artifacts
 
 .\build\nuget.exe pack src\Code52.i18n\NuSpec\Code52.i18n.nuspec -Version %VERSION% -OutputDirectory artifacts
+.\build\nuget.exe pack src\Code52.i18n.Web\NuSpec\Code52.i18n.Web.nuspec -Version %VERSION% -OutputDirectory artifacts
 .\build\nuget.exe pack src\Code52.i18n.MVC3.Logic\NuSpec\Code52.i18n.MVC3.Logic.nuspec -Version %VERSION% -OutputDirectory artifacts
-.\build\nuget.exe pack src\Code52.i18n.MVC3.Web\NuSpec\Code52.i18n.MVC3.Web.nuspec -Version %VERSION% -OutputDirectory artifacts
 .\build\nuget.exe pack src\Code52.i18n.MVC3.All\NuSpec\Code52.i18n.MVC3.All.nuspec -Version %VERSION% -OutputDirectory artifacts
 .\build\nuget.exe pack src\Code52.i18n.MVC4.Logic\NuSpec\Code52.i18n.MVC4.Logic.nuspec -Version %VERSION% -OutputDirectory artifacts
-.\build\nuget.exe pack src\Code52.i18n.MVC4.Web\NuSpec\Code52.i18n.MVC4.Web.nuspec -Version %VERSION% -OutputDirectory artifacts
 .\build\nuget.exe pack src\Code52.i18n.MVC4.All\NuSpec\Code52.i18n.MVC4.All.nuspec -Version %VERSION% -OutputDirectory artifacts
