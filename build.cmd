14e51d49a3b561bcb1fdbe36dd44c166ed45f3d6
Brendan Forster <me@brendanforster.com>
2012-03-03 17:23:09 +1100

    build script creates new packages

--- build.cmd
+++ build.cmd
@@ -9,3 +9,9 @@ if "%config%" == "" (
 )
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild build.proj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+mkdir artifacts
+
+.\build\nuget.exe pack src\Code52.i18n\NuSpec\Code52.i18n.nuspec -BasePath src\Code52.i18n\NuSpec\ -OutputDirectory artifacts
+.\build\nuget.exe pack src\Code52.i18n.MVC3\NuSpec\Code52.i18n.MVC3.nuspec -BasePath src\Code52.i18n.MVC3\NuSpec\ -OutputDirectory artifacts
+.\build\nuget.exe pack src\Code52.i18n.MVC4\NuSpec\Code52.i18n.MVC4.nuspec -BasePath src\Code52.i18n.MVC4\NuSpec\ -OutputDirectory artifacts
--- src/Code52.i18n/NuSpec/Content/Code/CultureHelper.cs.pp
+++ src/Code52.i18n/NuSpec/Content/Code/CultureHelper.cs.pp
@@ -1,4 +1,5 @@
-﻿namespace $rootnamespace$.Code52Code {
+﻿namespace $rootnamespace$.Code52Code 
+{
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
