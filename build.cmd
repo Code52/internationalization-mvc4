b408130fe305fc6bcc4e9076e089e9e7c70c0f7c
David Christiansen <coding@davedoes.net>
2012-03-03 19:26:20 +0000

    Build Code52.i18n.Web.MVC.Razor package

--- build.cmd
+++ build.cmd
@@ -17,6 +17,7 @@ rmdir artifacts /S /Q
 mkdir artifacts
 
 .\build\nuget.exe pack src\Code52.i18n\NuSpec\Code52.i18n.nuspec -Version %VERSION% -OutputDirectory artifacts
+.\build\nuget.exe pack src\Code52.i18n.Web.Razor\NuSpec\Code52.i18n.Web.MVC.Razor.nuspec -Version %VERSION% -OutputDirectory artifacts
 .\build\nuget.exe pack src\Code52.i18n.MVC\NuSpec\Code52.i18n.MVC3.nuspec -Version %VERSION% -OutputDirectory artifacts
 .\build\nuget.exe pack src\Code52.i18n.MVC\NuSpec\Code52.i18n.MVC4.nuspec -Version %VERSION% -OutputDirectory artifacts
 .\build\nuget.exe pack src\Code52.i18n.MVC3.Example\NuSpec\Code52.i18n.MVC3.Example.Razor.nuspec -Version %VERSION% -OutputDirectory artifacts
